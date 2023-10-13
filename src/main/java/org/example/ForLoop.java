36b3ea2a6f316cf3fe4dd01db9b3b90da84f6af2
Денис Филлер <denis.filler@yandex.ru>
2023-10-13 23:43:02 +0300

    loops

---
+++ src/main/java/org/example/ForLoop.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 1000; i >= 100; i--) {
+            if (i % 5 == 0 && i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ src/main/java/org/example/Loop.java
@@ -0,0 +1,45 @@
+package org.example;
+
+public class Loop {
+    public static void main(String[] args) {
+        int a = 10;
+        a = a + 1;
+        a += 1;
+        a++;
+        a = a - 1;
+        a -= 1;
+        a--;
+
+        int j = 1000;
+        while (j >= 0) {
+            j--;
+            if (j % 2 == 0) {
+                System.out.println(j);
+            }
+        }
+
+        int sum = 0;
+        int count = 0;
+
+
+        while (count <= 100) {
+            sum += count;
+            count++;
+        }
+        float result = sum / (float) count;
+        System.out.println(result);
+
+        int b = 0;
+        do {
+            System.out.println("Привет!");
+        }
+        while (b > 0);
+
+        for (int i = 1000; i > 0; i--) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+
+            }
+        }
+    }
+}
