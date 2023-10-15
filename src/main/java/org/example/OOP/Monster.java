f8100270ad5c7abbad1c80dcc99b76002646db76
Денис Филлер <denis.filler@yandex.ru>
2023-10-15 14:54:01 +0300

    OOP continue overpower Homework

---
+++ src/main/java/org/example/OOP/Monster.java
@@ -0,0 +1,46 @@
+package org.example.OOP;
+
+public class Monster {
+    int eyes;
+    int legs;
+    int hands;
+
+    Monster() {
+        this.eyes = 2;
+        this.legs = 2;
+        this.hands = 2;
+    }
+
+    Monster(int eyes) {
+        this.eyes = 3;
+        this.legs = 2;
+        this.hands = 2;
+    }
+
+    Monster(int eyes, int legs) {
+        this.eyes = 3;
+        this.legs = 2;
+        this.hands = 2;
+    }
+
+    Monster(int eyes, int legs, int hands) {
+        this.eyes = 3;
+        this.legs = 2;
+        this.hands = 2;
+    }
+
+    void voice() {
+        System.out.println("Голос");
+    }
+
+    void voice(int n) {
+        for (int i = 0; i < n; i++)
+            System.out.println("Голос");
+    }
+
+    void voice(int n, String word) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(word);
+        }
+    }
+}
