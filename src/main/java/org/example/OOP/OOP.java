c1ddd563478273eab92ae3273270e13f2605ce80
Денис Филлер <denis.filler@yandex.ru>
2023-10-21 17:39:13 +0300

    OOP key words + new class

--- src/main/java/org/example/OOP/OOP.java
+++ src/main/java/org/example/OOP/OOP.java
@@ -159,5 +159,17 @@ public class OOP {
         System.out.println(man.getAge());
         System.out.println(man.getName());
 
+        int square = MyMath.square(20);
+        System.out.println(square);
+        double length = MyMath.length(10);
+        System.out.println(length);
+        double area = MyMath.area(10);
+        System.out.println(area);
+        length = MyMath.length(10);
+        System.out.println(length);
+
+        int sum = MyMath.sum(1);
+        System.out.println(sum);
+
     }
 }
